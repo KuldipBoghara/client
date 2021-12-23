6aa0832868e1d17f6a3a7fc6bd42dee897a7e173
Mr-BK <bogharakuldip@gmail.com>
2021-12-23 11:46:14 -0500

    commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
 import { SIGN_IN, SIGN_OUT } from './types';
 
+import streams from '../apis/streams';
+
 export const signIn = (userId) => {
   return {
     type: SIGN_IN,
@@ -11,4 +13,10 @@ export const signOut = () => {
   return {
     type: SIGN_OUT
   };
+};
+
+//we are createing asynchronus action creator so using Thunk (function returns function)
+export const createStream = (formValues) => async (dispatch) => {
+  //POST request to server
+  streams.post('/streams', formValues);
 };
